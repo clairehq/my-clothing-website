6ef449afbf4bf99ea6241cce0d43de1025041891
clairehq <claireli.hq@gmail.com>
2020-07-13 17:52:00 -0700

    updating

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -7,7 +7,13 @@ export const selectCollections = createSelector(
     shop => shop.collections //input是shop的initial state，return的里面的collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    //get all keys off the object and give it in an array format, map to collections[key]
+    collections => Object.keys(collections).map(key => collections[key]) 
+)
+
 export const selectCollection = collectionUrlParam => createSelector(
     [selectCollections],
-    collections => collections[collectionUrlParam]
+    collections => collections[collectionUrlParam] //collectionUrlParam is a string
 )
